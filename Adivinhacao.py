90f3a09814c7efcc8783fdc1a3890804b80809ed
Jorge Lago <jorgelago92@gmail.com>
2022-09-19 16:16:32 -0300

    Adicionado o Break na Repetição

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,16 +1,31 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
 
+numero_secreto = 51
+numero_tentativas = 5
 
-numero_secreto= 42
+for rodada in range(1, numero_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, numero_tentativas))
+    chute_str = input("Digite um número entre 1 e 100: ")
+    print("Você digitou " , chute_str)
+    chute = int(chute_str)
 
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-chute_str = input("Digite o seu número: ")
-print("você digitou", chute_str)
+    if(acertou):
+        print("Você acertou!")
+        break
+    else:
+        if(maior):
+            print("Que Pena! Tente um número Menor na próxima vez!")
+        elif(menor):
+            print("Que Pena! Tente um número Maior na próxima vez!")
 
-chute= int(chute_str)
-
-if(numero_secreto == chute):
-    print("Você Acertoou !!")
-
-else:
-    print("Que Pena, tente de novo!!")
+print("Fim do jogo")
--- Readme.md
+++ Readme.md
@@ -7,6 +7,7 @@ Lógica do Jogo;
 Adicionar laço de Repetição;
 Resolvendo Problema de Repetição;
 Adicionando Break na Repetição;
+Adicionar Número aleatório ao *Número Secreto*;
 
 
 

bc03ffa1805408a09a4ad4b310cfc7944042039a
Jorge Lago <110360494+LaagoX@users.noreply.github.com>
2022-09-20 09:08:15 -0300

    Adicionando Pontos e número secreto no fim do jogo

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,12 +1,31 @@
+import random
+
+
+
+
 print("===========================================")
 print("JOGO DE ADIVINHAÇÃO COM PYTHON - PROJETO 01")
 print("===========================================")
 
-numero_secreto = 51
-numero_tentativas = 5
+pontos = 500
+numero_secreto = round(random.random() * 100)
+numero_tentativas = 0
+
+print("Qual o Nível de Dificuldade?")
+print("(1) Fácil   (2) Médio  (3) Difícil")
+
+nivel=int(input("Digite o número para definir nível:"))
+if nivel == 1:
+    numero_tentativas=10
+elif nivel == 2:
+    numero_tentativas=8
+else:
+    numero_tentativas=5
+
 
 for rodada in range(1, numero_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, numero_tentativas))
+    
     chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou " , chute_str)
     chute = int(chute_str)
@@ -20,12 +39,18 @@ for rodada in range(1, numero_tentativas + 1):
     menor   = chute < numero_secreto
 
     if(acertou):
-        print("Você acertou!")
+        print("Você acertou! e consegui incríveis ", pontos," pontos!!")
         break
     else:
         if(maior):
             print("Que Pena! Tente um número Menor na próxima vez!")
         elif(menor):
             print("Que Pena! Tente um número Maior na próxima vez!")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
+
+
+    if rodada >= numero_tentativas:
+        print("O número Secreto Era", numero_secreto)
 
 print("Fim do jogo")
